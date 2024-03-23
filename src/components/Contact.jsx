29208e36934525a2b382c117c441f3aa5e0e4dce
mundo88 <minhphung8898@gmail.com>
2024-03-24 04:51:38 +0700

    345345

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ContactImg from "../assets/images/contact-svg.svg"
 import useSocials from "../hooks/useSocials"
 import { TbArrowNarrowRight } from 'react-icons/tb';
+import { Link } from 'react-router-dom';
 
 const ContactForm = () => {
     return (
@@ -30,37 +31,41 @@ const ContactForm = () => {
 const ContactContent = ()=>{
     const socials = useSocials()
     return (
-        <div className='relative pt-36 overflow-hidden pr-12'>
-            <div className='w-full absolute top-0 right-0'>
-                <img src={ContactImg} alt=""  />
+        <div className='relative'>
+            <div className=" pt-36 overflow-hidden pr-12">
+                <div className='w-full absolute top-0 right-0'>
+                    <img src={ContactImg} alt=""  />
+                </div>
             </div>
-            <div className='relative bg-main pt-4'>
+            <div className='relative bg-main pt-4 w-fit'>
                 <div className="text-white text-7xl font-bold font-serif">
                     We'd love to hear your thoughts
                 </div>
-                <div className='mt-4 text-xl text-gray-300'>
+                <div className='mt-4 text-xl text-gray-300 pr-4'>
                     Tell us about your vision: which challanges are you facing? We'd love to stay in touch with you, so we are always ready to answer any question that interests you.
                 </div>
                 <p className='mt-8 text-white uppercase font-semibold mb-2 text-xl'>May tour Joint Stock company</p>
-                <div className='flex gap-12 '>
+                <div className='flex gap-12'>
                     <div className='space-y-2'>
-                        <p className='text-white uppercase font-semibold'>Address</p>
-                        <p className='text-gray-300'> No. 105, House 381, Lane 379 Minh Khai, Vinh Tuy Ward, Hai Ba Trung District, Hanoi City, Vietnam </p>
+                        <p className='text-white uppercase font-semibold text-sm'>Address</p>
+                        <p className='text-gray-300 text-sm'> No. 105, House 381, Lane 379 Minh Khai, Vinh Tuy Ward, Hai Ba Trung District, Hanoi City, Vietnam </p>
                     </div>
                     <div className='space-y-2'>
-                        <p className='text-white uppercase font-semibold'>PHONE</p>
-                        <p className='text-gray-300'>(+84) 902168187</p>
+                        <p className='text-white uppercase font-semibold text-sm'>PHONE</p>
+                        <Link to={'tel:+84902168187'} className='text-gray-300 text-sm whitespace-nowrap'>(+84) 902168187</Link>
                     </div>
                     <div className='space-y-2'>
-                        <p className='text-white uppercase font-semibold'>EMAIL</p>
-                        <p  className='text-gray-300'>contact@maytour.com</p>
+                        <p className='text-white uppercase font-semibold text-sm'>EMAIL</p>
+                        <Link to={"mailto:booking.maytour@gmail.com"}  className='text-gray-300 text-sm'>booking.maytour@gmail.com</Link>
                     </div>
                 </div>
                 <div className="mt-8 flex items-center gap-6">
                     {Object.keys(socials).map((key,i)=>(
-                        <button key={i} className='w-12 h-12 border-gray-500 border-2 flex items-center justify-center text-gray-300 hover:text-teal-300 hover:border-teal-300 active:-translate-y-1 duration-300'>
-                            {socials[key].icon}
-                        </button>
+                        <Link to={socials[key].to} key={i} className={`w-12 h-12 border-gray-500 ring-2 ring-gray-500 hover:ring-offset-[3px] hover:ring-offset-[#051214] flex items-center justify-center text-gray-300 relative ${socials[key].hoverClass} active:-translate-y-1 duration-300`}>
+                            <span className='relative z-10'>
+                                {socials[key].icon}
+                            </span>
+                        </Link>
                     ))}
                 </div>
             </div>
--- src/hooks/useSocials.jsx
+++ src/hooks/useSocials.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
-import { TbBrandFacebookFilled, TbBrandInstagram, TbBrandLinkedin, TbBrandTwitterFilled, TbBrandYoutubeFilled } from 'react-icons/tb';
+import { TbBrandFacebookFilled, TbBrandInstagram, TbBrandLinkedin, TbBrandTwitterFilled, TbBrandYoutubeFilled,TbBrandWhatsapp } from 'react-icons/tb';
 
 const useSocials = () => {
     const socials = {
         'facebook': {
-            'icon':<TbBrandFacebookFilled size={26}/>,
-            'url':'https://facebook.com/me'
+            'icon':<TbBrandFacebookFilled size={24}/>,
+            'to':'https://www.facebook.com/huong.lanvu87',
+            'hoverClass':'hover:bg-blue-600'
         },
         'instagram': {
-            'icon':<TbBrandInstagram size={26}/>,
-            'url':'https://facebook.com/me'
+            'icon':<TbBrandInstagram size={28}/>,
+            'to':'#',
+            'hoverClass':'after:bg-instagram after:inset-0 after:absolute after:opacity-0 hover:after:opacity-100 after:-z-1'
         },
         'twitter': {
-            'icon':<TbBrandTwitterFilled size={26}/>,
-            'url':'https://facebook.com/me'
+            'icon':<TbBrandTwitterFilled size={24}/>,
+            'to':'#',
+            'hoverClass':'hover:bg-sky-500'
         },
         'youtube': {
-            'icon':<TbBrandYoutubeFilled size={26}/>,
-            'url':'https://facebook.com/me'
+            'icon':<TbBrandYoutubeFilled size={24}/>,
+            'to':'#',
+            'hoverClass':'hover:bg-red-600'
         },
-        'linkedin': {
-            'icon':<TbBrandLinkedin size={26}/>,
-            'url':'https://facebook.com/me'
+        'whatsapp': {
+            'icon':<TbBrandWhatsapp size={24}/>,
+            'to':'https://wa.me/84902168187',
+            'hoverClass':'hover:bg-green-600'
         },
     }
     return socials
